2a5b8926ae358b09980b8e287b5594ceb9912df5
yurifilgueira <yurimedeiros141@gmail.com>
2024-08-07 22:32:34 -0200

    Feat: Update font

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -117,7 +118,7 @@ class LoginText extends StatelessWidget {
 
     return  Text(
       "Login",
-      style: TextStyle(color: Theme.of(context).colorScheme.onSurface, fontSize: 56),
+      style: GoogleFonts.bebasNeue(textStyle: TextStyle(color: Theme.of(context).colorScheme.onSurface, fontSize: 100)),
     );
   }
 }
